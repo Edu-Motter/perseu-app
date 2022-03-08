42415852175f9219c714fe8148bec683eb388a89
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-03-08 19:24:25 -0300

    feat: create component for training card

--- perseu/lib/src/screens/coach_screens/new_training_screening.dart
+++ perseu/lib/src/screens/coach_screens/new_training_screening.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:perseu/src/components/training_cards.dart';
 
 class NewTrainingScreen extends StatelessWidget {
   const NewTrainingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Color themeColor = Colors.teal;
-
     return Scaffold(
         appBar: AppBar(
           title: const Text('Novo treino'),
@@ -16,22 +15,7 @@ class NewTrainingScreen extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ExpansionPanelList(
-                  animationDuration: const Duration(seconds: 2),
-                  dividerColor: themeColor,
-                  elevation: 1,
-                  expandedHeaderPadding: const EdgeInsets.all((8)),
-                  children: [
-                    ExpansionPanel(
-                        headerBuilder: (context, isOpen) {
-                          return const Text("Hello");
-                        },
-                        body: const Text("Now Open!"),
-                        isExpanded: false),
-                  ],
-                )
-              ],
+              children: const [TrainingCard(), TrainingCard()],
             ),
           ),
         ]));
