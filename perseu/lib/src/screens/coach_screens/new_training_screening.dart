6c28f0631600566b558cb51eb5f4db41948fe4a9
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-03-08 21:18:40 -0300

    feat: add Speed Dial

--- perseu/lib/src/screens/coach_screens/new_training_screening.dart
+++ perseu/lib/src/screens/coach_screens/new_training_screening.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:perseu/src/components/training_list.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class NewTrainingScreen extends StatelessWidget {
   const NewTrainingScreen({Key? key}) : super(key: key);
@@ -9,6 +12,33 @@ class NewTrainingScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Novo treino'),
+        ),
+        // floatingActionButton: FloatingActionButton(
+        //   child: const Icon(Icons.menu),
+        //   onPressed: () {},
+        // ),
+        floatingActionButton: SpeedDial(
+          animatedIcon: AnimatedIcons.menu_close,
+          animatedIconTheme: const IconThemeData(size: 22.0),
+          // child: const Icon(Icons.add),
+          onOpen: () => print('OPENING DIAL'),
+          onClose: () => print('DIAL CLOSED'),
+          visible: true,
+          curve: Curves.bounceIn,
+          children: [
+            SpeedDialChild(
+              child: const Icon(Icons.add),
+              onTap: () => print('FIRST CHILD'),
+              label: 'Adicionar sessão',
+              labelStyle: const TextStyle(fontWeight: FontWeight.w500),
+            ),
+            SpeedDialChild(
+              child: const Icon(Icons.forward),
+              onTap: () => print('SECOND CHILD'),
+              label: 'Atribuir treino',
+              labelStyle: const TextStyle(fontWeight: FontWeight.w500),
+            ),
+          ],
         ),
         body: ListView(children: [
           Padding(
