4d5ff1465ccd312657b481c8e27a95b7b370127b
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-07-17 11:21:25 -0300

    feat: add checkbox list on assign training

--- perseu/lib/src/screens/coach_assign_training/assign_training_screen.dart
+++ perseu/lib/src/screens/coach_assign_training/assign_training_screen.dart
@@ -3,10 +3,7 @@ import 'package:flutter/material.dart';
 import 'assign_training_viewmodel.dart';
 
 class AssignTrainingScreen extends StatefulWidget {
-  const AssignTrainingScreen({Key? key, this.assignTrainingModel})
-      : super(key: key);
-
-  final AssignTrainingModel? assignTrainingModel;
+  const AssignTrainingScreen({Key? key}) : super(key: key);
   @override
   _AssignTrainingState createState() => _AssignTrainingState();
 }
@@ -15,10 +12,14 @@ class _AssignTrainingState extends State<AssignTrainingScreen> {
   late AssignTrainingModel assignTrainingModel;
   @override
   void initState() {
-    assignTrainingModel =
-        AssignTrainingModel(athleteName: "Zeca", athleteId: 0, assigned: false);
     super.initState();
   }
+
+  List<AssignTrainingModel> athletesToAssign = [
+    AssignTrainingModel(athleteName: "Rafael", athleteId: 0, assigned: false),
+    AssignTrainingModel(athleteName: "José", athleteId: 1, assigned: false),
+    AssignTrainingModel(athleteName: "Bianca", athleteId: 2, assigned: false),
+  ];
 
   final bool checkboxState = false;
   @override
@@ -29,20 +30,25 @@ class _AssignTrainingState extends State<AssignTrainingScreen> {
         ),
         body: ListView(padding: const EdgeInsets.all(16.0), children: [
           const Text("Atletas"),
-          CheckboxListTile(
-              title: Text(assignTrainingModel.athleteName),
-              value: assignTrainingModel.assigned,
-              onChanged: (bool? checkboxState) {
-                setState(() {
-                  assignTrainingModel.assigned = checkboxState ?? true;
-                });
-              }),
+          ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: athletesToAssign.length,
+            itemBuilder: (_, int index) {
+              return CheckboxListTile(
+                  title: Text(athletesToAssign[index].athleteName),
+                  value: athletesToAssign[index].assigned,
+                  onChanged: (bool? checkboxState) {
+                    setState(() {
+                      athletesToAssign[index].assigned = checkboxState ?? true;
+                    });
+                  });
+            },
+          ),
           const SizedBox(
             height: 16,
           ),
-          ElevatedButton(
-              onPressed: () {},
-              child: const Text('Atribuir'))
+          ElevatedButton(onPressed: () {}, child: const Text('Atribuir'))
         ]));
   }
 }
