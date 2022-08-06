e1af5674aea63694abbdfa37b384d8987ccade14
Eduardo Zen Motter <eduardo.motter@vmtecnologia.io>
2022-08-06 12:45:36 -0300

    feat: add new clients

---
+++ perseu/lib/src/services/clients/client_athlete.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:perseu/src/app/locator.dart';
+import 'package:perseu/src/services/foundation.dart';
+
+class ClientAthlete with ApiHelper {
+  final dio = locator.get<Dio>();
+
+  Future<Result<String>> createRequest(String requestCode, int athleteId) async {
+    return process(
+        dio.post('/api/criar-requisicao-equipe', data: {'codigo': requestCode, 'atleta': athleteId}),
+        onSuccess: (response) {
+          return Result.success(data: response.data['status']);
+        },
+        onError: (response) => const Result.error(
+            message: 'Falha ao realizar cadastro de equipe'));
+  }
+
+}
