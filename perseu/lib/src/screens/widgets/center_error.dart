22411d1fecc21afd99bf5f3c302c1006474082a4
Eduardo Zen Motter <eduardo.motter@vmtecnologia.io>
2022-10-29 15:53:57 -0300

    feat: new widgets

---
+++ perseu/lib/src/screens/widgets/center_error.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+
+class CenterError extends StatelessWidget {
+  const CenterError({
+    Key? key,
+    required this.message,
+  }) : super(key: key);
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        decoration: const BoxDecoration(
+            color: Colors.teal,
+            borderRadius: BorderRadius.all(Radius.circular(16))),
+        child: Text(
+          message,
+          textAlign: TextAlign.center,
+          style: const TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
