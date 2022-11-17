c2f6d14cd609822e965eac0dbcedbd56891b61ab
Eduardo Zen Motter <eduardo.motter@vmtecnologia.io>
2022-11-16 22:21:35 -0300

    feat: athlete details v1

--- perseu/lib/src/screens/athlete_trainings_details/athlete_trainings_details_screen.dart
+++ perseu/lib/src/screens/athlete_trainings_details/athlete_trainings_details_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:perseu/src/app/locator.dart';
+import 'package:perseu/src/models/dtos/athlete_info_dto.dart';
 import 'package:perseu/src/screens/training_details/training_details_screen.dart';
 import 'package:perseu/src/screens/training_to_athlete/training_to_athlete_screen.dart';
+import 'package:perseu/src/services/foundation.dart';
+import 'package:perseu/src/utils/date_formatters.dart';
+import 'package:perseu/src/utils/formatters.dart';
 import 'package:provider/provider.dart';
 
 import 'athlete_trainings_details_viewmodel.dart';
@@ -56,9 +60,29 @@ class AthleteTrainingsDetailsScreen extends StatelessWidget {
             ),
             body: CustomScrollView(
               slivers: [
-                const SliverToBoxAdapter(
-                  child: Placeholder(
-                    fallbackHeight: 140,
+                SliverToBoxAdapter(
+                  child: FutureBuilder(
+                    future: model.getAthleteInfo(athleteId),
+                    builder: (context,
+                        AsyncSnapshot<Result<AthleteInfoDTO>> snapshot) {
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.done:
+                          if (snapshot.hasData) {
+                            return AthleteInformationWithIcons(
+                                athlete: snapshot.data!.data!);
+                          } else {
+                            return const Center(
+                              child: Text('Falha ao carregar dados do usuário'),
+                            );
+                          }
+                        case ConnectionState.none:
+                        case ConnectionState.waiting:
+                        case ConnectionState.active:
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                      }
+                    },
                   ),
                 ),
                 const SliverToBoxAdapter(
@@ -139,6 +163,125 @@ class AthleteTrainingsDetailsScreen extends StatelessWidget {
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class AthleteInformationWithIcons extends StatelessWidget {
+  const AthleteInformationWithIcons({
+    Key? key,
+    required this.athlete,
+  }) : super(key: key);
+
+  static const standardStyle = TextStyle(color: Colors.white, fontSize: 16);
+  static const standardStyleBold =
+      TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold);
+
+  final AthleteInfoDTO athlete;
+  @override
+  Widget build(BuildContext context) {
+    final formattedCpf = Formatters.cpf().maskText(athlete.document);
+    final formattedDate = DateFormatters.toDateString(athlete.birthdate);
+    final formattedHeight =
+        Formatters.height().maskText(athlete.height.toString());
+    final formattedWeight =
+        Formatters.weight().maskText(athlete.weight.toString());
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        decoration: const BoxDecoration(
+            color: Colors.teal,
+            borderRadius: BorderRadius.all(Radius.circular(16))),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.person,
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                  const SizedBox(width: 8),
+                  const Text(
+                    'Nome: ',
+                    style: standardStyleBold,
+                  ),
+                  Text(athlete.name, style: standardStyle),
+                ],
+              ),
+              const Divider(color: Colors.white54,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.description,
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                  const SizedBox(width: 8),
+                  const Text(
+                    'CPF: ',
+                    style: standardStyleBold,
+                  ),
+                  Text(formattedCpf, style: standardStyle),
+                ],
+              ),
+              const Divider(color: Colors.white54,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.cake,
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                  const SizedBox(width: 8),
+                  const Text(
+                    'Data de nascimento: ',
+                    style: standardStyleBold,
+                  ),
+                  Text(formattedDate, style: standardStyle),
+                ],
+              ),
+              const Divider(color: Colors.white54,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.height,
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                  const SizedBox(width: 4),
+                  const Text(
+                    'Altura: ',
+                    style: standardStyleBold,
+                  ),
+                  Text('$formattedHeight m', style: standardStyle),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  const Icon(
+                    Icons.fitness_center,
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                  const SizedBox(width: 8),
+                  const Text(
+                    'Peso: ',
+                    style: standardStyleBold,
+                  ),
+                  Text('$formattedWeight Kg', style: standardStyle),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- perseu/lib/src/screens/athlete_trainings_details/athlete_trainings_details_viewmodel.dart
+++ perseu/lib/src/screens/athlete_trainings_details/athlete_trainings_details_viewmodel.dart
@@ -1,11 +1,14 @@
 import 'package:perseu/src/app/locator.dart';
+import 'package:perseu/src/models/dtos/athlete_info_dto.dart';
 import 'package:perseu/src/models/dtos/training_dto.dart';
+import 'package:perseu/src/services/clients/client_athlete.dart';
 import 'package:perseu/src/services/clients/client_coach.dart';
 import 'package:perseu/src/services/foundation.dart';
 import 'package:perseu/src/states/foundation.dart';
 
 class AthleteTrainingsDetailsViewModel extends AppViewModel {
   ClientCoach clientCoach = locator<ClientCoach>();
+  ClientAthlete clientAthlete = locator<ClientAthlete>();
 
   String get authToken => session.authToken!;
   int get teamId => session.userSession!.team!.id;
@@ -16,5 +19,14 @@ class AthleteTrainingsDetailsViewModel extends AppViewModel {
         await clientCoach.getTraining(authToken, trainingId);
     if (result.success) training = result.data;
     return result;
+  }
+
+  Future<Result<AthleteInfoDTO>> getAthleteInfo(int athleteId) async {
+    final result =
+    await clientAthlete.getAthlete(athleteId, session.authToken!);
+    if (result.success) {
+      return Result.success(data: AthleteInfoDTO.fromJson(result.data));
+    }
+    return Result.error(message: result.message);
   }
 }
