e2007c0fd1b7d705169e31efbaa64b4814f87736
Eduardo Zen Motter <eduardo.motter@vmtecnologia.io>
2022-12-20 20:52:23 -0300

    feat: fixing assets and removing unused files

--- perseu/lib/src/theme/app_images.dart
+++
@@ -1,3 +0,0 @@
-class AppImagens {
-  static String get imagem => 'assets/imagens/teste.png';
-}
--- perseu/lib/src/viewModels/login_view_model.dart
+++ perseu/lib/src/viewModels/login_view_model.dart
@@ -7,7 +7,7 @@ import '../states/foundation.dart';
 
 class LoginViewModel extends AppViewModel {
   static const gif = 'assets/gifs/fitness.gif';
-  static const image = 'assets/images/dumbell-accent.png';
+  static const image = 'assets/images/dumbbell-accent.png';
 
   String username = '';
   String password = '';
